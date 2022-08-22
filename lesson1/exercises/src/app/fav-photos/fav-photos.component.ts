586c00d26af8c85282215e9fa9a8fcfd48f2c8d3
Jessiklabs <jojacks90@gmail.com>
2022-08-22 10:20:37 -0500

    Completed initial edits to exercise1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i0.wp.com/jessiklabs.com/wp-content/uploads/2022/01/Linecooks.jpg';
+  image3 = 'https://i0.wp.com/jessiklabs.com/wp-content/uploads/2022/01/Soupkids.jpg';
 
   constructor() { }
 
